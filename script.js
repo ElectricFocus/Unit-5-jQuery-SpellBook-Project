a050c6c0de9ca62f96cf2087d708a7100870ce05
ElectricFocus <48684257+ElectricFocus@users.noreply.github.com>
2019-06-10 09:34:18 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#dark").click(function(){
+ $("#yes").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#lord  ").click(function(){
+ $("#game").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("  #rules   ").click(function(){
+  $("#bet").fadeToggle();
+});
+
+
+
